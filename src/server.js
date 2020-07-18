f1a997a6c33981196919e4dd068d64263da99d1c
Sharvin Shah <sharvinshah51@gmail.com>
2020-07-18 16:07:29 +0530

    Code Structuring

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
+
+const bootcamps = require("./routes/bootcamps");
 
 dotenv.config({
-    path: "./src/config/config.env"
+	path: "./src/config/config.env",
 });
 
 const app = express();
 
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+	PORT,
+	console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
