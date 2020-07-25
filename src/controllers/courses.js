b89afe723948fd89f600d21ba021533427411437
Sharvin Shah <sharvinshah51@gmail.com>
2020-07-25 12:39:51 +0530

    Implemented Ownership

--- src/controllers/courses.js
+++ src/controllers/courses.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
@@ -44,7 +45,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
-
+  req.body.user = req.user.id;
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
   if (!bootcamp) {
     return next(
@@ -54,7 +55,14 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
       )
     );
   }
-
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+        401
+      )
+    );
+  }
   const course = await Course.create(req.body);
   res.status(200).json({
     success: true,
@@ -70,6 +78,14 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(
       new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update course`,
+        401
+      )
     );
   }
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
@@ -93,8 +109,15 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
       new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
     );
   }
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete course`,
+        401
+      )
+    );
+  }
   await course.remove();
-
   res.status(200).json({
     success: true,
     data: {},
