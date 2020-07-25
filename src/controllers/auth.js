ff659a355bf584489a5258ef758f9f75ccf36918
Sharvin Shah <sharvinshah51@gmail.com>
2020-07-25 12:21:41 +0530

    Implemented Auth and Role Authorization

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,6 +2,24 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
+
 // @desc    Register user
 // @route   POST /api/v1/auth/register
 // @access  Public
@@ -13,8 +31,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
-  const token = user.getSignedJwtToken();
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc    Login user
@@ -26,16 +43,24 @@ exports.login = asyncHandler(async (req, res, next) => {
   if (!email || !password) {
     return next(new ErrorResponse("Please provide an email and password", 400));
   }
-
   const user = await User.findOne({ email }).select("+password");
   if (!user) {
     return next(new ErrorResponse("Invalid credentials", 401));
   }
-
   const isMatch = await user.matchPassword(password);
   if (!isMatch) {
     return next(new ErrorResponse("Invalid credentials", 401));
   }
-  const token = user.getSignedJwtToken();
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc    Get current logged in user
+// @route   GET /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 });
--- src/seeder.js
+++ src/seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 // Load Models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect database
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,13 +23,17 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`)
 );
+
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`));
+
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`));
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log(`Data imported...`.green.inverse);
     process.exit();
   } catch (error) {
@@ -42,6 +47,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log(`Data destroyed...`.red.inverse);
     process.exit();
   } catch (error) {
